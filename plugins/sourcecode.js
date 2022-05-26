627b73038fa05000bbdcc217d1e76e1dedbda621
Amaldx44 <106238395+Amaldx44@users.noreply.github.com>
2022-05-26 11:43:37 +0530

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,5 +1,5 @@
 let handler = async (m, {conn}) => {
-     conn.reply(m.chat, `This bot uses a github script\n\nhttps://github.com/SudoAnirudh/EvaMd(PUBLIC)\n\nGive Some Stars..`, m) 
+     conn.reply(m.chat, `This bot uses a github script\n\nhttps://ippo thara nokii erunoo(PUBLIC)\n\nGive Some Stars..`, m) 
 }
 handler.help = ['sourcecode']
 handler.tags = ['info']
